808ccf6804f33e6a0366c3ebf8d421f2d45653e5
Lunit567 <martin.lun.567@gmail.com>
2023-06-04 18:26:56 -0600

    doc3

--- src/controller/documentos.ts
+++ src/controller/documentos.ts
@@ -1,5 +1,17 @@
 import { RequestHandler } from "express";
 import {documentos} from "../models/documento";
+import {Sequelize} from "sequelize-typescript";
+import { Proyecto } from "../models/proyectos";
+import { Usuario } from "../models/usuarios";
+
+const connection = new Sequelize({
+    dialect: "mysql",
+    host: "localhost",
+    username: "root", 
+    password: "kaminomizo", 
+    database: "banco2",
+    port: 33061
+});
 
 export const adddoc:RequestHandler = async (req,res,next) =>{
     var doc = await documentos.create({...req.body});
@@ -15,5 +27,39 @@ export const dropdoc:RequestHandler = async (req,res,next) =>{
 
 export const obtdoc:RequestHandler = async (req,res,next) =>{
     const obtdocumento:documentos [] = await documentos.findAll();
-    return res.status(200).json({message: "documentos obtenidos", data:obtdocumento});   
+    return res.status(200).json({message: "Documentos obtenidos: "+obtdocumento.length, data:obtdocumento});   
+}
+
+export const obtdocID:RequestHandler = async (req,res,next) =>{
+    const {id} = req.params;
+    const docobtenido: documentos|null = await documentos.findByPk(id, {
+        include: Proyecto
+    });
+    return res.status(200).json({message: "Documento obtenido: ", data:docobtenido});   
+}
+
+export const obtuserdoc:RequestHandler = async (req,res,next) =>{
+    const userdoc = await Proyecto.findByPk(req.body.Proyecto_id,{
+        include:{model: Proyecto,
+            attributes: {exclude:["password", "email", "celular", "rol_id"]}},
+        attributes: {exclude:["id", "nombrep", "objetivos", "fechainicio", "fechafinal","usuario_codigo","carrera_clave"]}
+    });
+    return res.status(200).json({message: "Usuarios obtenidos: ", data:userdoc});   
+}
+
+export const upddoc: RequestHandler = async (req,res) => {
+    const {id} = req.params;
+    const docActualizado: documentos | null = await documentos.findByPk(id);
+    await documentos.update({...req.body}, {where:{id}});
+    return res.status(200).json({message:"Documento actualizado!",docActualizado});
+}
+
+export const obtexel:RequestHandler = async (req,res,next) =>{
+    var XLSX = require("xlsx");
+    const [results, metadata] = await connection.query('SELECT * FROM archivos_documentos');
+    const workSheet = XLSX.utils.json_to_sheet(results);
+    const workBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workBook, workSheet, "Sheet 2");
+    XLSX.writeFile(workBook, "C:\\Users\\marti\\Desktop\\Proyecto\\banco-api\\plantilla_2.xlsx");
+    return res.status(200).json({message: "Excel generado: ", data:results});   
 }
--- src/routes/documentos.ts
+++ src/routes/documentos.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { adddoc, dropdoc, obtdoc } from "../controller/documentos";
+import { adddoc, dropdoc, obtdoc, upddoc, obtuserdoc, obtexel, obtdocID } from "../controller/documentos";
 import { autorizar } from "../middleware/verify_token";
 
 const router = Router();
@@ -7,5 +7,9 @@ const router = Router();
 router.post("/", autorizar, adddoc);
 router.delete("/:id", autorizar, dropdoc);
 router.get("/", obtdoc);
+router.get("/Usuarios/:id", obtuserdoc);
+router.get("/ID/:id", obtdocID);
+router.get("/Exel", obtexel);
+router.put("/:id", upddoc);
 
 export default router;
